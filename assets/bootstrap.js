d282725b36a0d81de58c95b92d4832476aeb436e
IDMarinas <35842929+idmarinas@users.noreply.github.com>
2025-08-27 20:20:17 +0200

    chore: removed unnecessary semicolons in `bootstrap.js` for consistency

--- assets/bootstrap.js
+++ assets/bootstrap.js
@@ -1,5 +1,24 @@
-import { startStimulusApp } from '@symfony/stimulus-bundle';
+/**
+ * Copyright 2025 (C) IDMarinas - All Rights Reserved
+ *
+ * Last modified by "IDMarinas" on 27/08/2025, 20:20
+ *
+ * @project Foro de Ayuda y Soporte
+ * @see https://github.com/idmarinas/template-symfony
+ *
+ * @file bootstrap.js
+ * @date 27/08/2025
+ * @time 20:20
+ *
+ * @author Iván Diaz Marinas (IDMarinas)
+ * @license BSD 3-Clause License
+ *
+ * @since 1.0.0
+ */
 
-const app = startStimulusApp();
+import {startStimulusApp} from '@symfony/stimulus-bundle'
+
+const app = startStimulusApp()
+
 // register any custom, 3rd party controllers here
 // app.register('some_controller_name', SomeImportedController);
