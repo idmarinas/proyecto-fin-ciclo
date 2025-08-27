e1bf33b5e742ea3af963cf1fcacc5adc76040eff
IDMarinas <35842929+idmarinas@users.noreply.github.com>
2025-08-27 20:17:09 +0200

    chore: actualizado autoload_runtime y closure en `index.php` para incluir `APP_ID`

--- public/index.php
+++ public/index.php
@@ -1,9 +1,24 @@
 <?php
+/**
+ * Copyright 2025 (C) IDMarinas - All Rights Reserved
+ *
+ * Last modified by "IDMarinas" on 27/08/2025, 20:17
+ *
+ * @project Foro de Ayuda y Soporte
+ * @see     https://github.com/idmarinas/template-symfony
+ *
+ * @file    index.php
+ * @date    27/08/2025
+ * @time    20:17
+ *
+ * @author  Iván Diaz Marinas (IDMarinas)
+ * @license BSD 3-Clause License
+ *
+ * @since   1.0.0
+ */
 
 use App\Kernel;
 
-require_once dirname(__DIR__).'/vendor/autoload_runtime.php';
+require_once dirname(__DIR__) . '/vendor/autoload_runtime.php';
 
-return function (array $context) {
-    return new Kernel($context['APP_ENV'], (bool) $context['APP_DEBUG']);
-};
+return fn(array $context) => new Kernel($context['APP_ENV'], (bool)$context['APP_DEBUG'], $context['APP_ID']);
