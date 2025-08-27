2aebcfb5afc11dac99d3f01c95480b06d93b1189
IDMarinas <35842929+idmarinas@users.noreply.github.com>
2025-08-27 20:22:20 +0200

    chore(config): adjusted spacing in `importmap.php` for improved readability

--- importmap.php
+++ importmap.php
@@ -1,4 +1,21 @@
 <?php
+/**
+ * Copyright 2025 (C) IDMarinas - All Rights Reserved
+ *
+ * Last modified by "IDMarinas" on 27/08/2025, 20:22
+ *
+ * @project Foro de Ayuda y Soporte
+ * @see     https://github.com/idmarinas/template-symfony
+ *
+ * @file    importmap.php
+ * @date    27/08/2025
+ * @time    20:22
+ *
+ * @author  Iván Diaz Marinas (IDMarinas)
+ * @license BSD 3-Clause License
+ *
+ * @since   1.0.0
+ */
 
 /**
  * Returns the importmap for this application.
@@ -12,17 +29,17 @@
  * The "importmap:require" command can be used to add new entries to this file.
  */
 return [
-    'app' => [
-        'path' => './assets/app.js',
+    'app'                      => [
+        'path'       => './assets/app.js',
         'entrypoint' => true,
     ],
-    '@hotwired/stimulus' => [
+    '@hotwired/stimulus'       => [
         'version' => '3.2.2',
     ],
     '@symfony/stimulus-bundle' => [
         'path' => './vendor/symfony/stimulus-bundle/assets/dist/loader.js',
     ],
-    '@hotwired/turbo' => [
+    '@hotwired/turbo'          => [
         'version' => '7.3.0',
     ],
 ];
