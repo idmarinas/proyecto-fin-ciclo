5bcf0dfb45be20672365b6e60af2403d8c158443
IDMarinas <35842929+idmarinas@users.noreply.github.com>
2025-08-28 10:22:08 +0200

    chore(code): añadir atributo `#[Override]` y declarar uso de la clase `Override` en `Kernel.php`

--- src/Kernel.php
+++ src/Kernel.php
@@ -2,10 +2,10 @@
 /**
  * Copyright 2025 (C) IDMarinas - All Rights Reserved
  *
- * Last modified by "IDMarinas" on 10/06/2025, 19:18
+ * Last modified by "IDMarinas" on 28/08/2025, 10:21
  *
  * @project Foro de Ayuda y Soporte
- * @see     https://github.com/idmarinas/proyecto-fin-ciclo
+ * @see     https://github.com/idmarinas/template-symfony
  *
  * @file    Kernel.php
  * @date    30/05/2025
@@ -19,6 +19,7 @@
 
 namespace App;
 
+use Override;
 use Symfony\Bundle\FrameworkBundle\Kernel\MicroKernelTrait;
 use Symfony\Component\HttpKernel\Kernel as BaseKernel;
 
@@ -26,6 +27,7 @@ class Kernel extends BaseKernel
 {
     use MicroKernelTrait;
 
+    #[Override]
     public function getProjectDir (): string
     {
         return dirname(__DIR__);
