704081eb099fc935e1cf7882660487d2f16741fe
IDMarinas <35842929+idmarinas@users.noreply.github.com>
2025-09-06 20:40:07 +0200

    chore(docker): añadir script `BuildDockerImageDEV.ps1` para construir imágenes Docker en entorno de desarrollo

---
+++ .docker/scripts/BuildDockerImageDEV.ps1
@@ -0,0 +1,22 @@
+function Get-Env
+{
+    param([string]$VarName, [string]$File = "$PSScriptRoot/../../.env")
+
+    $line = Get-Content $File | Where-Object { $_ -match "^$VarName\s*=" } | Select-Object -First 1
+
+    if ($line)
+    {
+        $value = ($line -split "=", 2)[1].Trim()
+        return $value -replace '^["\x27]|["\x27]$', ''
+    }
+    return $null
+}
+
+$appVersion = Get-Env -VarName "APP_VERSION"
+$appTitle = Get-Env -VarName "APP_TITLE"
+
+Write-Host "DEV Contruyendo la imagen Docker" -BackgroundColor Red
+Write-Host "$appTitle" -BackgroundColor Green
+Write-Host "Tag: $appVersion" -BackgroundColor Blue
+
+docker build --target dev -f .docker/Dockerfile -t "idmarinas/pfc:$appVersion-dev" .
