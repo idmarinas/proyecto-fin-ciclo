8fc7bbd44cf474877ac64120b27c7920c36fb74c
IDMarinas <35842929+idmarinas@users.noreply.github.com>
2025-09-09 12:49:36 +0200

    chore(deployer): añadir tarea `deploy:upload_files` para subir archivos al servidor

---
+++ .deployer/task/upload_files.php
@@ -0,0 +1,37 @@
+<?php
+/**
+ * Copyright 2025 (C) IDMarinas - All Rights Reserved
+ *
+ * Last modified by "IDMarinas" on 07/09/2025, 20:45
+ *
+ * @project Foro de Ayuda y Soporte
+ * @see     https://github.com/idmarinas/proyecto-fin-ciclo
+ *
+ * @file    upload_files.php
+ * @date    05/03/2025
+ * @time    11:48
+ *
+ * @author  Iván Diaz Marinas (IDMarinas)
+ * @license BSD 3-Clause License
+ *
+ * @since   1.0.0
+ */
+
+namespace Deployer;
+
+import('recipe/common.php');
+
+desc('Upload files to server');
+task('deploy:upload_files', function () {
+    writeln('<fg=blue>Subiendo archivos a {{text_prod}}...</>');
+    upload('./', '{{release_path}}', [
+        'flags'   => '-azPh',
+        'options' => [
+            '--include=compose.yaml',
+            '--include=compose.prod.yaml',
+            '--exclude=**/*',
+        ],
+    ]);
+    writeln('<fg=blue>Subiendo idmarinas_pfc_{{app_version}}.tar a {{text_prod}}...</>');
+    upload('./.deployer/idmarinas_pfc_{{app_version}}.tar', '{{release_path}}');
+});
