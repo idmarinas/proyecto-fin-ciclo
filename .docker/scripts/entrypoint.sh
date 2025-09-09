571a5073ccff65cffc1c5277f8bc21b615b609f3
IDMarinas <35842929+idmarinas@users.noreply.github.com>
2025-09-09 12:33:56 +0200

    chore(docker): añadir script de entrada `entrypoint.sh` para configurar y ejecutar contenedor

---
+++ .docker/scripts/entrypoint.sh
@@ -0,0 +1,23 @@
+#!/bin/sh
+
+##
+# Copyright 2025 (C) IDMarinas - All Rights Reserved
+#
+# Last modified by "IDMarinas" on 09/09/2025, 10:34
+#
+# @project Foro de Ayuda y Soporte
+# @see https://github.com/idmarinas/proyecto-fin-ciclo
+#
+# @file entrypoint.sh
+# @date 09/09/2025
+# @time 10:28
+#
+# @author Iván Diaz Marinas (IDMarinas)
+# @license BSD 3-Clause License
+#
+# @since 1.0.0
+#
+
+dockerd &
+docker-php-entrypoint
+exec frankenphp run --config '/etc/frankenphp/Caddyfile' "$@"
