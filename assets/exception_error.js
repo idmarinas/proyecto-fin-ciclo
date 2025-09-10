a8fd3f8ad80e7a96d34342e65ca6d96e4b57fe98
IDMarinas <35842929+idmarinas@users.noreply.github.com>
2025-09-10 11:19:24 +0200

    chore(assets): añadir `exception_error.js` como nuevo archivo de script

---
+++ assets/exception_error.js
@@ -0,0 +1,19 @@
+/**
+ * Copyright 2025 (C) IDMarinas - All Rights Reserved
+ *
+ * Last modified by "IDMarinas" on 10/09/2025, 11:07
+ *
+ * @project Foro de Ayuda y Soporte
+ * @see https://github.com/idmarinas/proyecto-fin-ciclo
+ *
+ * @file exception_error.js
+ * @date 02/09/2025
+ * @time 14:20
+ *
+ * @author Iván Diaz Marinas (IDMarinas)
+ * @license BSD 3-Clause License
+ *
+ * @since 1.0.0
+ */
+
+import './styles/exception_error.css';
