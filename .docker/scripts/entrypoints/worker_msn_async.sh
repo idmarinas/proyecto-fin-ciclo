b0b623d82a56803f5cf4f0929eea8fb430e31383
IDMarinas <35842929+idmarinas@users.noreply.github.com>
2025-09-11 10:32:20 +0200

    chore(docker): añadir entrypoints para workers `scheduler` y `async` de Symfony Messenger

---
+++ .docker/scripts/entrypoints/worker_msn_async.sh
@@ -0,0 +1,22 @@
+#!/bin/sh
+
+##
+# Copyright 2025 (C) IDMarinas - All Rights Reserved
+#
+# Last modified by "IDMarinas" on 11/09/2025, 10:23
+#
+# @project Foro de Ayuda y Soporte
+# @see https://github.com/idmarinas/proyecto-fin-ciclo
+#
+# @file worker_msn_async.sh
+# @date 11/09/2025
+# @time 10:22
+#
+# @author Iván Diaz Marinas (IDMarinas)
+# @license BSD 3-Clause License
+#
+# @since 1.0.0
+#
+
+docker-php-entrypoint
+php bin/console messenger:consume async --time-limit=3600 --memory-limit=128M --limit=100
