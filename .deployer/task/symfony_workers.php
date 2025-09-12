98dcc28daa84aacac9bfc970b447b1301ba2a472
IDMarinas <35842929+idmarinas@users.noreply.github.com>
2025-09-12 13:56:06 +0200

    chore(deploy): eliminar tareas de gestión de workers de Symfony en el flujo de despliegue

--- .deployer/task/symfony_workers.php
+++ .deployer/task/symfony_workers.php
@@ -2,7 +2,7 @@
 /**
  * Copyright 2025 (C) IDMarinas - All Rights Reserved
  *
- * Last modified by "IDMarinas" on 12/09/2025, 12:57
+ * Last modified by "IDMarinas" on 12/09/2025, 13:55
  *
  * @project Foro de Ayuda y Soporte
  * @see     https://github.com/idmarinas/proyecto-fin-ciclo
@@ -50,52 +50,3 @@ task('deploy:symfony:workers:stop', function () {
         }
     }
 });
-
-//
-
-//
-
-//
-
-//
-
-//
-
-//
-
-//
-
-//
-
-//
-
-desc('Stop works of previous release');
-task('remote:prod:workers:stop', function () {
-    writeln('<info>Stop workers of <fg=red;options=bold>previous</> release in {{local_prod_text}}</info>');
-    within('{{previous_release}}', function () {
-        run('php bin/console cron:stop', real_time_output: true);
-        run('php bin/console messenger:stop-workers', real_time_output: true);
-    });
-});
-
-desc('Start works of current release');
-task('remote:prod:workers:start', function () {
-    writeln('<info>Start workers of <fg=green;options=bold>current</> release in {{local_prod_text}}</info>');
-    within('{{release_path}}', function () {
-        run('php bin/console cron:start > /dev/null 2>&1 &');
-    });
-});
-
-desc('Workers manager');
-task('remote:prod:workers', [
-    'remote:prod:workers:stop',
-    'remote:prod:workers:start',
-]);
-
-//- exec: { cmd: 'php bin/console cron:stop', desc: 'Stop Cron scheduler'}
-//- exec: { cmd: 'php bin/console messenger:stop-workers', desc: 'Se paran todos los workers del messenger' }
-
-//
-// Hooks
-//
-before('deploy:publish', 'remote:prod:workers');
