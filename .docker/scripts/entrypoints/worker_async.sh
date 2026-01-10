38e847a41d0a770ce5ef443b6e4f87b85a7e3fd6
IDMarinas <35842929+idmarinas@users.noreply.github.com>
2026-01-10 11:22:24 +0100

    chore(scripts): eliminar comentarios redundantes en scripts de entrada de Docker

--- .docker/scripts/entrypoints/worker_async.sh
+++ .docker/scripts/entrypoints/worker_async.sh
@@ -1,16 +1,4 @@
 #!/bin/sh
 
-##
-# Copyright 2025 (C) IDMarinas - All Rights Reserved
-#
-#
-#
-# @file worker_async.sh
-#
-# @author Iván Diaz Marinas (IDMarinas)
-# @license proprietary
-#
-#
-
 docker-php-entrypoint
 php bin/console messenger:consume async --time-limit=3600 --memory-limit=128M --limit=100
--- .docker/scripts/entrypoints/worker_scheduler.sh
+++ .docker/scripts/entrypoints/worker_scheduler.sh
@@ -1,17 +1,5 @@
 #!/bin/sh
 
-##
-# Copyright 2025 (C) IDMarinas - All Rights Reserved
-#
-#
-#
-# @file worker_scheduler.sh
-#
-# @author Iván Diaz Marinas (IDMarinas)
-# @license proprietary
-#
-#
-
 # Scheduler parece que no es compatible con la opción --limit
 docker-php-entrypoint
 php bin/console messenger:consume scheduler_default --time-limit=3600 --memory-limit=128M
