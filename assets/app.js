5c334366ce6f41b8cf26b036ea4a2b7f4271343f
IDMarinas <35842929+idmarinas@users.noreply.github.com>
2026-01-10 11:08:00 +0100

    chore(assets): eliminar comentarios redundantes y reemplazar bootstrap.js por stimulus_bootstrap.js

--- assets/app.js
+++ assets/app.js
@@ -1,22 +1,22 @@
 /**
- * Copyright 2025 (C) IDMarinas - All Rights Reserved
+ * Copyright 2026 (C) IDMarinas - All Rights Reserved
  *
- * Last modified by "IDMarinas" on 10/09/2025, 11:02
+ * Last modified by "IDMarinas" on 10/01/2026, 11:07
  *
  * @project Foro de Ayuda y Soporte
  * @see https://github.com/idmarinas/proyecto-fin-ciclo
  *
  * @file app.js
- * @date 10/09/2025
- * @time 11:20
+ * @date 10/01/2026
+ * @time 11:07
  *
  * @author Iván Diaz Marinas (IDMarinas)
- * @license BSD 3-Clause License
+ * @license proprietary
  *
  * @since 1.0.0
  */
 
-import './bootstrap.js';
+import './stimulus_bootstrap.js';
 /*
  * Welcome to your app's main JavaScript file!
  *
