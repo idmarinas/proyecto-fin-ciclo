7a82cfcb1d453d606921bff18647ccbc546fafe5
IDMarinas <35842929+idmarinas@users.noreply.github.com>
2026-01-10 21:39:42 +0100

    feat(repository): agregar UserRepository con soporte para actualización de contraseñas mediante Doctrine

---
+++ src/Repository/User/UserRepository.php
@@ -0,0 +1,52 @@
+<?php
+/**
+ * Copyright 2026 (C) IDMarinas - All Rights Reserved
+ *
+ * Last modified by "IDMarinas" on 10/01/2026, 21:40
+ *
+ * @project Foro de Ayuda y Soporte
+ * @see     https://github.com/idmarinas/proyecto-fin-ciclo
+ *
+ * @file    UserRepository.php
+ * @date    10/01/2026
+ * @time    21:39
+ *
+ * @author  Iván Diaz Marinas (IDMarinas)
+ * @license proprietary
+ *
+ * @since   1.0.0
+ */
+
+namespace App\Repository\User;
+
+use App\Entity\User\User;
+use Doctrine\Bundle\DoctrineBundle\Repository\ServiceEntityRepository;
+use Doctrine\Persistence\ManagerRegistry;
+use Symfony\Component\Security\Core\Exception\UnsupportedUserException;
+use Symfony\Component\Security\Core\User\PasswordAuthenticatedUserInterface;
+use Symfony\Component\Security\Core\User\PasswordUpgraderInterface;
+
+/**
+ * @extends ServiceEntityRepository<User>
+ */
+final class UserRepository extends ServiceEntityRepository implements PasswordUpgraderInterface
+{
+    public function __construct (ManagerRegistry $registry)
+    {
+        parent::__construct($registry, User::class);
+    }
+
+    /**
+     * Used to upgrade (rehash) the user's password automatically over time.
+     */
+    public function upgradePassword (PasswordAuthenticatedUserInterface $user, string $newHashedPassword): void
+    {
+        if (!$user instanceof User) {
+            throw new UnsupportedUserException(sprintf('Instances of "%s" are not supported.', $user::class));
+        }
+
+        $user->setPassword($newHashedPassword);
+        $this->getEntityManager()->persist($user);
+        $this->getEntityManager()->flush();
+    }
+}
