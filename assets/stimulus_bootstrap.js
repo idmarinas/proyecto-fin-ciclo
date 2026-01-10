ea48d6f4ca0f31c8872334a840e88a091c671853
IDMarinas <35842929+idmarinas@users.noreply.github.com>
2026-01-10 11:05:08 +0100

    chore(assets): renombrar bootstrap.js a stimulus_bootstrap.js y eliminar comentarios redundantes

--- assets/stimulus_bootstrap.js
+++ assets/stimulus_bootstrap.js
@@ -1,26 +1,25 @@
 /**
- * Copyright 2025 (C) IDMarinas - All Rights Reserved
+ * Copyright 2026 (C) IDMarinas - All Rights Reserved
  *
- * Last modified by "IDMarinas" on 14/09/2025, 11:28
+ * Last modified by "IDMarinas" on 10/01/2026, 11:04
  *
  * @project Foro de Ayuda y Soporte
  * @see https://github.com/idmarinas/proyecto-fin-ciclo
  *
- * @file bootstrap.js
- * @date 27/08/2025
- * @time 20:20
+ * @file stimulus_bootstrap.js
+ * @date 10/01/2026
+ * @time 11:05
  *
  * @author Iván Diaz Marinas (IDMarinas)
- * @license BSD 3-Clause License
+ * @license proprietary
  *
  * @since 1.0.0
  */
 
-import {startStimulusApp} from '@symfony/stimulus-bundle'
+import { startStimulusApp } from '@symfony/stimulus-bundle'
 import registerIdmUiBundle from '@idmarinas/ui-bundle'
 
 const app = startStimulusApp()
 registerIdmUiBundle(app)
-
 // register any custom, 3rd party controllers here
 // app.register('some_controller_name', SomeImportedController);
