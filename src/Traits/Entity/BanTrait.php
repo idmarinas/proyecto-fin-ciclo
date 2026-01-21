4b99cd7c6877be18bc68a16d5d1308cc6db1ad1a
IDMarinas <35842929+idmarinas@users.noreply.github.com>
2026-01-21 23:12:49 +0100

    feat(entity): añadir `BanTrait` para gestionar estados de baneo

---
+++ src/Traits/Entity/BanTrait.php
@@ -0,0 +1,56 @@
+<?php
+/**
+ * Copyright 2026 (C) IDMarinas - All Rights Reserved
+ *
+ * Last modified by "IDMarinas" on 20/01/2026, 22:52
+ *
+ * @project Foro de Ayuda y Soporte
+ * @see     https://github.com/idmarinas/proyecto-fin-ciclo
+ *
+ * @file    BanTrait.php
+ * @date    20/01/2026
+ * @time    22:52
+ *
+ * @author  Iván Diaz Marinas (IDMarinas)
+ * @license proprietary
+ *
+ * @since   1.0.0
+ */
+
+namespace App\Traits\Entity;
+
+use DateTime;
+use DateTimeInterface;
+use Doctrine\DBAL\Types\Types;
+use Doctrine\ORM\Mapping as ORM;
+
+trait BanTrait
+{
+    /** Date on which the ban is ended */
+    #[ORM\Column(type: Types::DATETIME_MUTABLE, nullable: true)]
+    protected ?DateTimeInterface $bannedUntil = null;
+
+    public function getBannedUntil (): ?DateTimeInterface
+    {
+        return $this->bannedUntil;
+    }
+
+    public function setBannedUntil (?DateTimeInterface $bannedUntil): static
+    {
+        $this->bannedUntil = $bannedUntil;
+
+        return $this;
+    }
+
+    /**
+     * Gets whether the user is locked out.
+     */
+    public function isBanned (): bool
+    {
+        if (!$this->bannedUntil instanceof DateTimeInterface || '-0001-11-30' === $this->bannedUntil->format('Y-m-d')) {
+            return false;
+        }
+
+        return $this->bannedUntil > (new DateTime('now'));
+    }
+}
