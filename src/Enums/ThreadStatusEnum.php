1f8a1534bf3dd728bd2431cd8b28ba2d6a20b13c
IDMarinas <35842929+idmarinas@users.noreply.github.com>
2026-01-22 23:32:39 +0100

    feat(enums): añadir `ThreadStatusEnum` para gestionar estados de hilos en el foro

---
+++ src/Enums/ThreadStatusEnum.php
@@ -0,0 +1,30 @@
+<?php
+/**
+ * Copyright 2026 (C) IDMarinas - All Rights Reserved
+ *
+ * Last modified by "IDMarinas" on 22/01/2026, 22:56
+ *
+ * @project Foro de Ayuda y Soporte
+ * @see     https://github.com/idmarinas/proyecto-fin-ciclo
+ *
+ * @file    ThreadStatusEnum.php
+ * @date    22/01/2026
+ * @time    22:47
+ *
+ * @author  Iván Diaz Marinas (IDMarinas)
+ * @license proprietary
+ *
+ * @since   1.0.0
+ */
+
+namespace App\Enums;
+
+enum ThreadStatusEnum: string
+{
+    case OPEN             = 'open';
+    case IN_REVIEW        = 'in_review';
+    case WAITING_CUSTOMER = 'waiting_customer';
+    case WAITING_SUPPORT  = 'waiting_support';
+    case RESOLVED         = 'resolved';
+    case CLOSED           = 'closed';
+}
