ff804b2de09144da8c849a14501f22286f5f0be0
IDMarinas <35842929+idmarinas@users.noreply.github.com>
2026-01-26 20:15:22 +0100

    feat(controller): añadir atributos `Seo` y `Sitemap`, actualizar ruta y limpiar métodos en `HomeController`

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -1,18 +1,36 @@
 <?php
+/**
+ * Copyright 2026 (C) IDMarinas - All Rights Reserved
+ *
+ * Last modified by "IDMarinas" on 26/01/2026, 20:14
+ *
+ * @project Foro de Ayuda y Soporte
+ * @see     https://github.com/idmarinas/proyecto-fin-ciclo
+ *
+ * @file    HomeController.php
+ * @date    10/01/2026
+ * @time    21:13
+ *
+ * @author  Iván Diaz Marinas (IDMarinas)
+ * @license proprietary
+ *
+ * @since   1.0.0
+ */
 
 namespace App\Controller;
 
+use Idm\Bundle\Seo\Attributes\Seo;
+use Idm\Bundle\Seo\Attributes\Sitemap;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 
 final class HomeController extends AbstractController
 {
-    #[Route('/', name: 'app_home')]
-    public function index(): Response
+    #[Sitemap, Seo]
+    #[Route('/', name: 'home', methods: ['GET'])]
+    public function index (): Response
     {
-        return $this->render('pages/home/index.html.twig', [
-            'controller_name' => 'HomeController',
-        ]);
+        return $this->render('pages/home/index.html.twig', []);
     }
 }
