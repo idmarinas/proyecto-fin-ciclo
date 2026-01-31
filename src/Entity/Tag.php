c8cd44b71fbf7dd16c3025a773f6ded60460e42b
IDMarinas <35842929+idmarinas@users.noreply.github.com>
2026-01-31 19:50:51 +0100

    feat(templates): añadir soporte para nuevos enlaces en el menú de usuario

--- src/Entity/Tag.php
+++ src/Entity/Tag.php
@@ -2,7 +2,7 @@
 /**
  * Copyright 2026 (C) IDMarinas - All Rights Reserved
  *
- * Last modified by "IDMarinas" on 25/01/2026, 13:28
+ * Last modified by "IDMarinas" on 31/01/2026, 19:50
  *
  * @project Foro de Ayuda y Soporte
  * @see     https://github.com/idmarinas/proyecto-fin-ciclo
