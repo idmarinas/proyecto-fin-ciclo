cb3b56afc91a97db68c1a4bac261ca3461e9edc5
IDMarinas <35842929+idmarinas@users.noreply.github.com>
2026-02-01 23:40:40 +0100

    feat(admin): traducir elementos del menú y configurar iconos para acciones CRUD

--- src/Controller/Admin/DashboardController.php
+++ src/Controller/Admin/DashboardController.php
@@ -2,7 +2,7 @@
 /**
  * Copyright 2025-2026 (C) IDMarinas - All Rights Reserved
  *
- * Last modified by "IDMarinas" on 25/01/2026, 13:58
+ * Last modified by "IDMarinas" on 31/01/2026, 23:06
  *
  * @project Foro de Ayuda y Soporte
  * @see     https://github.com/idmarinas/proyecto-fin-ciclo
@@ -19,6 +19,7 @@
 
 namespace App\Controller\Admin;
 
+use Admin\Enums\Crud\ActionsEnum;
 use App\Entity\Forum;
 use App\Entity\Forum\Message;
 use App\Entity\Forum\MessageAttachment;
@@ -29,6 +30,9 @@ use App\Entity\Subscription;
 use App\Entity\Tag;
 use App\Entity\User\User;
 use EasyCorp\Bundle\EasyAdminBundle\Attribute\AdminDashboard;
+use EasyCorp\Bundle\EasyAdminBundle\Config\Action;
+use EasyCorp\Bundle\EasyAdminBundle\Config\Actions;
+use EasyCorp\Bundle\EasyAdminBundle\Config\Crud;
 use EasyCorp\Bundle\EasyAdminBundle\Config\Dashboard;
 use EasyCorp\Bundle\EasyAdminBundle\Config\MenuItem;
 use EasyCorp\Bundle\EasyAdminBundle\Controller\AbstractDashboardController;
@@ -77,31 +81,54 @@ final class DashboardController extends AbstractDashboardController
         yield from parent::configureMenuItems();
 
         // Forum Management Section
-        yield MenuItem::section('Forum Management');
+        yield MenuItem::section('Gestión del foro');
 
-        yield MenuItem::linkToCrud('Forums', 'fa fa-comments', Forum::class);
+        yield MenuItem::linkToCrud('Foro', 'fa fa-comments', Forum::class);
 
-        yield MenuItem::linkToCrud('Threads', 'fa fa-list', Thread::class);
+        yield MenuItem::linkToCrud('Hilos', 'fa fa-list', Thread::class);
 
-        yield MenuItem::linkToCrud('Messages', 'fa fa-comment', Message::class);
+        yield MenuItem::linkToCrud('Mensajes', 'fa fa-comment', Message::class);
 
-        yield MenuItem::linkToCrud('Attachments', 'fa fa-paperclip', MessageAttachment::class);
+        yield MenuItem::linkToCrud('Adjuntos', 'fa fa-paperclip', MessageAttachment::class);
 
         yield MenuItem::linkToCrud('Reactions', 'fa fa-smile', MessageReaction::class);
 
-        yield MenuItem::linkToCrud('Tags', 'fa fa-tags', Tag::class);
+        yield MenuItem::linkToCrud('Etiquetas', 'fa fa-tags', Tag::class);
 
         // User Management Section
-        yield MenuItem::section('User Management');
+        yield MenuItem::section('Gestión de usuarios');
 
-        yield MenuItem::linkToCrud('Users', 'fa fa-users', User::class);
+        yield MenuItem::linkToCrud('Usuarios', 'fa fa-users', User::class);
 
-        yield MenuItem::linkToCrud('Subscriptions', 'fa fa-bookmark', Subscription::class);
+        yield MenuItem::linkToCrud('Suscripciones', 'fa fa-bookmark', Subscription::class);
 
         // Moderation Section
-        yield MenuItem::section('Moderation');
+        yield MenuItem::section('Moderación');
 
-        yield MenuItem::linkToCrud('Reports', 'fa fa-flag', Report::class);
+        yield MenuItem::linkToCrud('Informes', 'fa fa-flag', Report::class);
     }
 
+    #[Override]
+    public function configureActions (): Actions
+    {
+        $detail = fn(Action $action) => $action->setIcon('fas fa-eye');
+        $edit = fn(Action $action) => $action->setIcon('fas fa-pen-to-square');
+        $delete = fn(Action $action) => $action->setIcon('fas fa-trash-can text-danger');
+        $save = fn(Action $action) => $action->setIcon('fa-solid fa-floppy-disk');
+        $new = fn(Action $action) => $action->setIcon('fas fa-square-plus me-1');
+
+        return parent::configureActions()
+            ->add(Crud::PAGE_INDEX, Action::DETAIL)
+            ->add(Crud::PAGE_NEW, Action::INDEX)
+            ->add(Crud::PAGE_EDIT, Action::DETAIL)
+            ->add(Crud::PAGE_EDIT, Action::DELETE)
+            ->update(Crud::PAGE_INDEX, Action::NEW, $new)
+            ->update(Crud::PAGE_INDEX, Action::EDIT, $edit)
+            ->update(Crud::PAGE_INDEX, Action::DETAIL, $detail)
+            ->update(Crud::PAGE_INDEX, Action::DELETE, $delete)
+            ->update(Crud::PAGE_EDIT, Action::DETAIL, $detail)
+            ->update(Crud::PAGE_EDIT, Action::SAVE_AND_RETURN, $save)
+            ->update(Crud::PAGE_DETAIL, Action::EDIT, $edit)
+        ;
+    }
 }
