d77db529c00873968ea752d5c3c3968efcf06147
IDMarinas <35842929+idmarinas@users.noreply.github.com>
2026-02-08 19:32:42 +0100

    refactor(security): añadir atributo `#[Override]` al método `checkPreAuth` en `AdminChecker`

--- src/Security/Checker/AdminChecker.php
+++ src/Security/Checker/AdminChecker.php
@@ -2,7 +2,7 @@
 /**
  * Copyright 2026 (C) IDMarinas - All Rights Reserved
  *
- * Last modified by "IDMarinas" on 30/01/2026, 20:26
+ * Last modified by "IDMarinas" on 08/02/2026, 19:32
  *
  * @project Foro de Ayuda y Soporte
  * @see     https://github.com/idmarinas/proyecto-fin-ciclo
@@ -20,6 +20,7 @@
 namespace App\Security\Checker;
 
 use App\Entity\User\User;
+use Override;
 use Symfony\Component\Security\Core\Authentication\Token\PreAuthenticatedToken;
 use Symfony\Component\Security\Core\Exception\AccessDeniedException;
 use Symfony\Component\Security\Core\Exception\CustomUserMessageAccountStatusException;
@@ -27,6 +28,7 @@ use Symfony\Component\Security\Core\User\UserInterface;
 
 final class AdminChecker extends AbstractChecker
 {
+    #[Override]
     public function checkPreAuth (UserInterface $user): void
     {
         /** @var User $user */
