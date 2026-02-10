201538ca92c50eea110231a40a7b56f5b15d63fe
IDMarinas <35842929+idmarinas@users.noreply.github.com>
2026-02-10 23:21:30 +0100

    feat(tests): añadir fixtures de usuarios y eliminar archivo obsoleto en 1.x

---
+++ tests/DataFixtures/User/UserFixtures.php
@@ -0,0 +1,61 @@
+<?php
+/**
+ * Copyright 2026 (C) IDMarinas - All Rights Reserved
+ *
+ * Last modified by "IDMarinas" on 10/02/2026, 23:21
+ *
+ * @project Foro de Ayuda y Soporte
+ * @see     https://github.com/idmarinas/proyecto-fin-ciclo
+ *
+ * @file    UserFixtures.php
+ * @date    03/02/2026
+ * @time    23:24
+ *
+ * @author  Iván Diaz Marinas (IDMarinas)
+ * @license proprietary
+ *
+ * @since   1.0.0
+ */
+
+namespace App\Tests\DataFixtures\User;
+
+use App\Tests\Enums\UserEnum;
+use App\Tests\Factory\User\UserFactory;
+use DateMalformedStringException;
+use DateTime;
+use Doctrine\Bundle\FixturesBundle\Fixture;
+use Doctrine\Persistence\ObjectManager;
+use Symfony\Component\Yaml\Yaml;
+
+final class UserFixtures extends Fixture
+{
+    /**
+     * @throws DateMalformedStringException
+     */
+    public function load (ObjectManager $manager): void
+    {
+        // Cargar YAML
+        $users = Yaml::parseFile(dirname(__DIR__) . '/data/dummy_users_expanded.yaml');
+
+        foreach ($users['users'] as $user) {
+            $createdAt = new DateTime($user['created_at']);
+            $entity = UserFactory::new()
+                ->updatedAt($createdAt)
+                ->createOne([
+                    'username'     => $user['username'],
+                    'email'        => $user['email'],
+                    'roles'        => $user['roles'],
+                    'password'     => UserEnum::{str_replace('ROLE_', '', $user['roles'][0])}->getPassword(),
+                    'avatar'       => $user['avatar'],
+                    'isVerified'   => $user['is_verified'],
+                    'reputation'   => $user['reputation'],
+                    'signature'    => $user['bio'] ?? '',
+                    'createdAt'    => $createdAt,
+                    'client'       => in_array('ROLE_CLIENT', $user['roles']),
+                    'lastActiveAt' => new DateTime($user['last_login']),
+                ])
+            ;
+            self::addReference('user_' . $user['id'], $entity);
+        }
+    }
+}
