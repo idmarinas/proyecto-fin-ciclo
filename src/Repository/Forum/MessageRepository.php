df805dd1ab59fa516592c72cbe65057b4ccfcd92
IDMarinas <35842929+idmarinas@users.noreply.github.com>
2026-02-18 22:48:28 +0100

    refactor(repository): agregar método `findAllByThread` en `MessageRepository` en 1.x

--- src/Repository/Forum/MessageRepository.php
+++ src/Repository/Forum/MessageRepository.php
@@ -2,7 +2,7 @@
 /**
  * Copyright 2026 (C) IDMarinas - All Rights Reserved
  *
- * Last modified by "IDMarinas" on 21/01/2026, 23:19
+ * Last modified by "IDMarinas" on 17/02/2026, 14:12
  *
  * @project Foro de Ayuda y Soporte
  * @see     https://github.com/idmarinas/proyecto-fin-ciclo
@@ -20,10 +20,28 @@
 namespace App\Repository\Forum;
 
 use App\Entity\Forum\Message;
+use App\Entity\Forum\Thread;
 use Doctrine\Bundle\DoctrineBundle\Repository\ServiceEntityRepository;
-use Gedmo\Tree\Entity\Repository\NestedTreeRepository;
+use Doctrine\ORM\QueryBuilder;
+use Doctrine\Persistence\ManagerRegistry;
 
 /**
  * @extends ServiceEntityRepository<Message>
  */
-final class MessageRepository extends NestedTreeRepository {}
+final class MessageRepository extends ServiceEntityRepository
+{
+    public function __construct (ManagerRegistry $registry)
+    {
+        parent::__construct($registry, Message::class);
+    }
+
+    public function findAllByThread (Thread $thread): QueryBuilder
+    {
+        return $this
+            ->createQueryBuilder('m')
+            ->where('m.thread = :thread')
+            ->orderBy('m.createdAt', 'ASC')
+            ->setParameter('thread', $thread)
+        ;
+    }
+}
