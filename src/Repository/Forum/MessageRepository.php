38998950b046c39471a4a3753f4c42cbc094dfa9
IDMarinas <35842929+idmarinas@users.noreply.github.com>
2026-02-19 23:42:07 +0100

    feat(repository): añadir métodos `countMessagesForThread` y `findLastMessageForThread` en `MessageRepository` en 1.x

--- src/Repository/Forum/MessageRepository.php
+++ src/Repository/Forum/MessageRepository.php
@@ -2,19 +2,19 @@
 /**
  * Copyright 2026 (C) IDMarinas - All Rights Reserved
  *
- * Last modified by "IDMarinas" on 17/02/2026, 14:12
+ * Last modified by "IDMarinas" on 19/02/2026, 21:50
  *
  * @project Foro de Ayuda y Soporte
- * @see     https://github.com/idmarinas/proyecto-fin-ciclo
+ * @see https://github.com/idmarinas/proyecto-fin-ciclo
  *
- * @file    MessageRepository.php
- * @date    21/01/2026
- * @time    23:19
+ * @file MessageRepository.php
+ * @date 21/01/2026
+ * @time 23:19
  *
- * @author  Iván Diaz Marinas (IDMarinas)
+ * @author Iván Diaz Marinas (IDMarinas)
  * @license proprietary
  *
- * @since   1.0.0
+ * @since 1.0.0
  */
 
 namespace App\Repository\Forum;
@@ -42,6 +42,39 @@ final class MessageRepository extends ServiceEntityRepository
             ->where('m.thread = :thread')
             ->orderBy('m.createdAt', 'ASC')
             ->setParameter('thread', $thread)
+        ;
+    }
+
+    /**
+     * Cuenta los mensajes visibles (no eliminados por SoftDelete) de un hilo.
+     */
+    public function countMessagesForThread (Thread $thread): int
+    {
+        return (int)$this
+            ->createQueryBuilder('m')
+            ->select('COUNT(m.id)')
+            ->where('m.thread = :thread')
+            ->setParameter('thread', $thread)
+            ->getQuery()
+            ->getSingleScalarResult()
+        ;
+    }
+
+    /**
+     * Devuelve el último mensaje visible (no eliminado por SoftDelete) de un hilo.
+     * Devuelve null si el hilo no tiene ningún mensaje.
+     */
+    public function findLastMessageForThread (Thread $thread): ?Message
+    {
+        return $this
+            ->createQueryBuilder('m')
+            ->where('m.thread = :thread')
+            ->andWhere('m.deletedAt IS NULL')
+            ->orderBy('m.createdAt', 'DESC')
+            ->setMaxResults(1)
+            ->setParameter('thread', $thread)
+            ->getQuery()
+            ->getOneOrNullResult()
         ;
     }
 }
