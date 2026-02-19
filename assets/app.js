10d845a36d03127b29b214fb7bed74427561bbe8
IDMarinas <35842929+idmarinas@users.noreply.github.com>
2026-02-19 23:42:26 +0100

    feat(repository, js): añadir métodos en `MessageRepository` y gestionar imágenes de avatar dinámicamente en 1.x
    - Añadidos `countMessagesForThread` y `findLastMessageForThread` en `MessageRepository`
    - Gestionar carga y errores de imágenes de avatar en `app.js`

--- assets/app.js
+++ assets/app.js
@@ -1,7 +1,7 @@
 /**
  * Copyright 2026 (C) IDMarinas - All Rights Reserved
  *
- * Last modified by "IDMarinas" on 11/01/2026, 19:35
+ * Last modified by "IDMarinas" on 19/02/2026, 23:21
  *
  * @project Foro de Ayuda y Soporte
  * @see https://github.com/idmarinas/proyecto-fin-ciclo
@@ -24,3 +24,49 @@ import './stimulus_bootstrap.js';
  * which should already be in your base.html.twig.
  */
 import './styles/app.css';
+
+
+document.addEventListener("DOMContentLoaded", () => {
+    document.querySelectorAll('[data-slot="avatar-image"]').forEach(img => {
+        const onLoad = () => {
+            img.classList.remove("hidden");
+            img.closest('[data-slot="avatar"]')
+                ?.querySelector('[data-slot="avatar-fallback"]')
+                ?.classList.add("hidden");
+        };
+
+        const onError = () => {
+            // Aquí decides qué hacer si falla la imagen
+            img.classList.add("hidden");
+            img.closest('[data-slot="avatar"]')
+                ?.querySelector('[data-slot="avatar-fallback"]')
+                ?.classList.remove("hidden");
+        };
+
+        img.addEventListener("load", onLoad);
+        img.addEventListener("error", onError);
+
+        // Si ya estaba cargada antes de añadir el listener
+        if (img.complete && img.naturalWidth !== 0) {
+            onLoad();
+        }
+
+        // Si ya estaba en estado de error antes de añadir el listener
+        if (img.complete && img.naturalWidth === 0) {
+            onError();
+        }
+    });
+
+
+});
+
+/*
+
+<img
+    data-slot="avatar-image"
+    class="{{ 'hidden rounded-full aspect-square size-full object-cover ' ~ attributes.render('class')|tailwind_merge }}"
+    onload="this.classList.remove('hidden'); this.closest('[data-slot=avatar]')?.querySelector('[data-slot=avatar-fallback]')?.classList.add('hidden');"
+    {{ attributes.defaults({alt: ''}) }}
+/>
+
+*/
