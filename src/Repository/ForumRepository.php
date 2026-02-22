d44317c0f37fef94168f9027ef83d1232d56832f
IDMarinas <35842929+idmarinas@users.noreply.github.com>
2026-02-22 13:16:21 +0100

    feat(repository): sobrescribir métodos para incluir jerarquía de nodos con fecha del último mensaje en `ForumRepository` en 1.x
    - Añadido `getNodesHierarchyQueryBuilder` para incluir `lastMessageAt`.
    - Ajustado `getNodesHierarchy` para aplanar resultados mezclados.

--- src/Repository/ForumRepository.php
+++ src/Repository/ForumRepository.php
@@ -2,7 +2,7 @@
 /**
  * Copyright 2026 (C) IDMarinas - All Rights Reserved
  *
- * Last modified by "IDMarinas" on 01/02/2026, 16:19
+ * Last modified by "IDMarinas" on 22/02/2026, 13:16
  *
  * @project Foro de Ayuda y Soporte
  * @see     https://github.com/idmarinas/proyecto-fin-ciclo
@@ -22,8 +22,44 @@ namespace App\Repository;
 use App\Entity\Forum;
 use Doctrine\Bundle\DoctrineBundle\Repository\ServiceEntityRepository;
 use Gedmo\Tree\Entity\Repository\NestedTreeRepository;
+use Override;
 
 /**
  * @extends ServiceEntityRepository<Forum>
  */
-final class ForumRepository extends NestedTreeRepository {}
+final class ForumRepository extends NestedTreeRepository
+{
+
+    #[Override]
+    public function getNodesHierarchyQueryBuilder(
+        $node = null,
+        $direct = false,
+        array $options = [],
+        $includeNode = false
+    ) {
+        $qb = parent::getNodesHierarchyQueryBuilder($node, $direct, $options, $includeNode);
+
+        $qb
+            ->addSelect('m.createdAt AS lastMessageAt')
+            ->leftJoin('node.lastThread', 'lt')
+            ->leftJoin('lt.lastMessage', 'm')
+        ;
+
+        return $qb;
+    }
+
+    #[Override]
+    public function getNodesHierarchy($node = null, $direct = false, array $options = [], $includeNode = false)
+    {
+        $results = parent::getNodesHierarchy($node, $direct, $options, $includeNode);
+
+        // Aplanamos el resultado si Doctrine lo ha devuelto en formato mixto debido al addSelect
+        return array_map(function (array $item) {
+            if (isset($item[0]) && is_array($item[0])) {
+                return array_merge($item[0], array_diff_key($item, [0 => true]));
+            }
+
+            return $item;
+        }, $results);
+    }
+}
