11ef3d12253362a54df41e7fe1abee70faf83ed0
IDMarinas <35842929+idmarinas@users.noreply.github.com>
2026-02-26 21:49:59 +0100

    refactor(repository): ajustar formato y espaciado en MessageRepository en 1.x

--- src/Repository/Forum/MessageRepository.php
+++ src/Repository/Forum/MessageRepository.php
@@ -2,19 +2,19 @@
 /**
  * Copyright 2026 (C) IDMarinas - All Rights Reserved
  *
- * Last modified by "IDMarinas" on 19/02/2026, 21:50
+ * Last modified by "IDMarinas" on 26/02/2026, 21:49
  *
  * @project Foro de Ayuda y Soporte
- * @see https://github.com/idmarinas/proyecto-fin-ciclo
+ * @see     https://github.com/idmarinas/proyecto-fin-ciclo
  *
- * @file MessageRepository.php
- * @date 21/01/2026
- * @time 23:19
+ * @file    MessageRepository.php
+ * @date    21/01/2026
+ * @time    23:19
  *
- * @author Iván Diaz Marinas (IDMarinas)
+ * @author  Iván Diaz Marinas (IDMarinas)
  * @license proprietary
  *
- * @since 1.0.0
+ * @since   1.0.0
  */
 
 namespace App\Repository\Forum;
@@ -30,12 +30,12 @@ use Doctrine\Persistence\ManagerRegistry;
  */
 final class MessageRepository extends ServiceEntityRepository
 {
-    public function __construct (ManagerRegistry $registry)
+    public function __construct(ManagerRegistry $registry)
     {
         parent::__construct($registry, Message::class);
     }
 
-    public function findAllByThread (Thread $thread): QueryBuilder
+    public function findAllByThread(Thread $thread): QueryBuilder
     {
         return $this
             ->createQueryBuilder('m')
@@ -48,7 +48,7 @@ final class MessageRepository extends ServiceEntityRepository
     /**
      * Cuenta los mensajes visibles (no eliminados por SoftDelete) de un hilo.
      */
-    public function countMessagesForThread (Thread $thread): int
+    public function countMessagesForThread(Thread $thread): int
     {
         return (int)$this
             ->createQueryBuilder('m')
@@ -64,12 +64,11 @@ final class MessageRepository extends ServiceEntityRepository
      * Devuelve el último mensaje visible (no eliminado por SoftDelete) de un hilo.
      * Devuelve null si el hilo no tiene ningún mensaje.
      */
-    public function findLastMessageForThread (Thread $thread): ?Message
+    public function findLastMessageForThread(Thread $thread): ?Message
     {
         return $this
             ->createQueryBuilder('m')
             ->where('m.thread = :thread')
-            ->andWhere('m.deletedAt IS NULL')
             ->orderBy('m.createdAt', 'DESC')
             ->setMaxResults(1)
             ->setParameter('thread', $thread)
