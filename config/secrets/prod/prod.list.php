360b10970b69edd50cbc79d92415dea40c1520a3
IDMarinas <35842929+idmarinas@users.noreply.github.com>
2026-03-07 20:35:52 +0100

    feat(secrets): añadir configuración inicial para APP_SECRET en 1.x
    - Creado archivo de secreto `prod.APP_SECRET.1cf811.php` para entorno de producción.
    - Actualizado `prod.list.php` para incluir `APP_SECRET` en la lista de secretos.

--- config/secrets/prod/prod.list.php
+++ config/secrets/prod/prod.list.php
@@ -1,22 +1,23 @@
 <?php
 /**
- * Copyright 2025 (C) IDMarinas - All Rights Reserved
+ * Copyright 2026 (C) IDMarinas - All Rights Reserved
  *
- * Last modified by "IDMarinas" on 10/09/2025, 18:11
+ * Last modified by "IDMarinas" on 07/03/2026, 20:35
  *
  * @project Foro de Ayuda y Soporte
  * @see     https://github.com/idmarinas/proyecto-fin-ciclo
  *
  * @file    prod.list.php
- * @date    10/09/2025
- * @time    18:12
+ * @date    07/03/2026
+ * @time    20:35
  *
  * @author  Iván Diaz Marinas (IDMarinas)
- * @license BSD 3-Clause License
+ * @license proprietary
  *
  * @since   1.0.0
  */
 
 return [
+    'APP_SECRET'           => null,
     'CLOUDFLARE_API_TOKEN' => null,
 ];
