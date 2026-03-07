6187981fe54cacb60a870cc586f3230109dfaa13
IDMarinas <35842929+idmarinas@users.noreply.github.com>
2026-03-07 21:59:44 +0100

    feat(secrets): agregar `DATABASE_USER` y `DATABASE_PASSWORD` a la lista de secretos en 1.x

--- config/secrets/prod/prod.list.php
+++ config/secrets/prod/prod.list.php
@@ -2,14 +2,14 @@
 /**
  * Copyright 2026 (C) IDMarinas - All Rights Reserved
  *
- * Last modified by "IDMarinas" on 07/03/2026, 20:35
+ * Last modified by "IDMarinas" on 07/03/2026, 21:42
  *
  * @project Foro de Ayuda y Soporte
  * @see     https://github.com/idmarinas/proyecto-fin-ciclo
  *
  * @file    prod.list.php
  * @date    07/03/2026
- * @time    20:35
+ * @time    21:59
  *
  * @author  Iván Diaz Marinas (IDMarinas)
  * @license proprietary
@@ -20,4 +20,6 @@
 return [
     'APP_SECRET'           => null,
     'CLOUDFLARE_API_TOKEN' => null,
+    'DATABASE_PASSWORD'    => null,
+    'DATABASE_USER'        => null,
 ];
