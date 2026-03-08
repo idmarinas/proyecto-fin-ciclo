4de65915ddd2036312da4ed490ad615f793a718c
IDMarinas <35842929+idmarinas@users.noreply.github.com>
2026-03-08 12:26:47 +0100

    feat(secrets): añadir soporte para `MAILER_DSN` en configuración de secretos en 1.x
    - Incluido el secreto `MAILER_DSN` en el archivo `prod.list.php` para la gestión de datos de conexión del correo.
    - Añadido archivo encriptado `prod.MAILER_DSN.97e321.php` correspondiente a la configuración de producción.

--- config/secrets/prod/prod.list.php
+++ config/secrets/prod/prod.list.php
@@ -2,14 +2,14 @@
 /**
  * Copyright 2026 (C) IDMarinas - All Rights Reserved
  *
- * Last modified by "IDMarinas" on 07/03/2026, 21:42
+ * Last modified by "IDMarinas" on 08/03/2026, 12:26
  *
  * @project Foro de Ayuda y Soporte
  * @see     https://github.com/idmarinas/proyecto-fin-ciclo
  *
  * @file    prod.list.php
- * @date    07/03/2026
- * @time    21:59
+ * @date    08/03/2026
+ * @time    12:26
  *
  * @author  Iván Diaz Marinas (IDMarinas)
  * @license proprietary
@@ -22,4 +22,5 @@ return [
     'CLOUDFLARE_API_TOKEN' => null,
     'DATABASE_PASSWORD'    => null,
     'DATABASE_USER'        => null,
+    'MAILER_DSN'           => null,
 ];
