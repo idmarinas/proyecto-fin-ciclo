3c30f3e8b68df9fc096bb4914b22b55f89a6e8c6
IDMarinas <35842929+idmarinas@users.noreply.github.com>
2026-03-08 02:13:46 +0100

    feat(tests): añadir pruebas para rutas principales y legales en 1.x
    - Incorporadas pruebas para las rutas `/`, `/forums`, y las páginas legales (`/aviso-legal`, `/politica-de-privacidad`, `/terminos-y-condiciones`, `/politica-de-cookies`).
    - Verificado el correcto funcionamiento de las respuestas y redirecciones.

--- tests/Controller/HomeControllerTest.php
+++ tests/Controller/HomeControllerTest.php
@@ -1,4 +1,21 @@
 <?php
+/**
+ * Copyright 2026 (C) IDMarinas - All Rights Reserved
+ *
+ * Last modified by "IDMarinas" on 08/03/2026, 01:33
+ *
+ * @project Foro de Ayuda y Soporte
+ * @see     https://github.com/idmarinas/proyecto-fin-ciclo
+ *
+ * @file    HomeControllerTest.php
+ * @date    08/03/2026
+ * @time    02:13
+ *
+ * @author  Iván Diaz Marinas (IDMarinas)
+ * @license proprietary
+ *
+ * @since   1.0.0
+ */
 
 namespace App\Tests\Controller;
 
@@ -10,6 +27,50 @@ final class HomeControllerTest extends WebTestCase
     {
         $client = static::createClient();
         $client->request('GET', '/');
+
+        self::assertResponseIsSuccessful();
+    }
+
+    public function testForums(): void
+    {
+        $client = static::createClient();
+        $client->request('GET', '/forums');
+
+        self::assertResponseRedirects('/forums/');
+
+        $client->followRedirect();
+
+        self::assertResponseIsSuccessful();
+    }
+
+    public function testLegal(): void
+    {
+        $client = static::createClient();
+        $client->request('GET', '/legal/aviso-legal');
+
+        self::assertResponseIsSuccessful();
+    }
+
+    public function testPrivacy(): void
+    {
+        $client = static::createClient();
+        $client->request('GET', '/legal/politica-de-privacidad');
+
+        self::assertResponseIsSuccessful();
+    }
+
+    public function testTerms(): void
+    {
+        $client = static::createClient();
+        $client->request('GET', '/legal/terminos-y-condiciones');
+
+        self::assertResponseIsSuccessful();
+    }
+
+    public function testCookies(): void
+    {
+        $client = static::createClient();
+        $client->request('GET', '/legal/politica-de-cookies');
 
         self::assertResponseIsSuccessful();
     }
